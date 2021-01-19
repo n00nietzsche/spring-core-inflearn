9b3991e30f7d5b08bf45d29663ea139c7dfe1266
n00nietzsche <n00nietzsche@gmail.com>
2021-01-19 20:04:40 +0900

    AppConfig 리팩토링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,14 +11,25 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
+    /*
+    나중에 구현체가 바뀔 때는 위의 구현체를 반환하는 부분만 바꿔주면 된다.
+     */
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
         return new OrderServiceImpl(
-                new MemoryMemberRepository()
-                , new FixDiscountPolicy()
+                memberRepository()
+                , discountPolicy()
         );
     }
 }
