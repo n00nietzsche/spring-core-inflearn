d9bbfc2e52de366159cec149274ee6cec2f20aea
n00nietzsche <n00nietzsche@gmail.com>
2021-02-03 16:52:57 +0900

    스프링 핵심 원리 - 기본편 #57 프로토타입 스코프 - 싱글톤 빈과 함께 사용시 문제점

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,12 +1,17 @@
 package hello.core.lifecycle;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+// javax 에서 지원하는 것은 자바 진영에서 공식적으로 지원하는 어노테이션이다.
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메세지");
     }
 
     public void setUrl(String url) {
@@ -18,12 +23,27 @@ public class NetworkClient {
         System.out.println("connect: " + url);
     }
 
+    // 서비스 종료 시 호출
+    public void disconnect() {
+        System.out.println("close " + url);
+    }
+
     public void call(String message) {
         System.out.println("call: " + url + " message = " + message);
     }
 
-    // 서비스 종료 시 호출
-    public void disconnect() {
-        System.out.println("close " + url);
+
+    // 스프링 의존관계 주입이 끝나고 호출
+    @PostConstruct
+    public void initialize() throws Exception {
+        System.out.println("NetworkClient.initialize");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @PreDestroy
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
+        disconnect();
     }
 }
---
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,41 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class SingletonTest {
+    @Test
+    void singletonBeanFind() {
+        AnnotationConfigApplicationContext applicationContext
+                = new AnnotationConfigApplicationContext(SingletonBean.class);
+
+        SingletonBean singletonBean1 = applicationContext.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = applicationContext.getBean(SingletonBean.class);
+
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+
+        Assertions.assertThat(singletonBean1).isSameAs(singletonBean2);
+
+        applicationContext.close();
+    }
+
+    // 사실 기본 값이라 안적어줘도 됨
+    @Scope("singleton")
+    static class SingletonBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+}
